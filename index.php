d2721052715dfb49ae6ed1db7a02b54af57ad6bf
StudentBart <538364@edu.rocmn.nl>
2020-08-26 17:25:33 +0200

    mobile fix

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $low_temp = $crud->custom_query("SELECT MIN(temperature) FROM `hum_temp` where `
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="functions.js"></script>
     <script type="text/javascript" src="core.js"></script>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
